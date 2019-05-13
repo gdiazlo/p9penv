eec77b796998a1dd5fd8042a3db6027528ad49f3
Gabriel Díaz <gdiazlo@macslack.slackware.es>
2019-05-14 00:00:05 +0200

    clean up font download script

--- assets/getfonts.sh
+++ assets/getfonts.sh
@@ -4,76 +4,75 @@ DEST=~/.fonts
 
 syntax() {
 	fonts=("https://www.wfonts.com/download/data/2016/07/06/syntax-lt-std/syntax-lt-std.zip")
-	install "syntax" "${fonts[@]}"
+	for i in $fonts; do
+		install "syntax" "${fonts[@]}"
+	done
 }
 
 lucida() {
-	fonts=("https://www.wfonts.com/download/data/2014/11/24/lucida-sans-unicode/lucida-sans-unicode.zip" "https://www.wfonts.com/download/data/2016/07/08/lucida-sans/lucida-sans.zip" "https://www.wfonts.com/download/data/2014/12/30/lucida-sans-typewriter/lucida-sans-typewriter.zip" "https://www.wfonts.com/download/data/2015/10/29/lucida-grande/lucida-grande.zip" "https://www.wfonts.com/download/data/2014/12/30/lucida-calligraphy/lucida-calligraphy.zip" "https://www.wfonts.com/download/data/2014/12/30/lucida-fax/lucida-fax.zip" "https://www.wfonts.com/download/data/2016/05/14/lucida-bright/lucida-bright.zip" "https://www.ffonts.net/Lucida-Console.font.zip" "https://www.wfonts.com/download/data/2016/07/10/letter-gothic-std/letter-gothic-std.zip")
+	fonts=("https://www.wfonts.com/download/data/2014/11/24/lucida-sans-unicode/lucida-sans-unicode.zip" "https://www.wfonts.com/download/data/2016/07/08/lucida-sans/lucida-sans.zip" "https://www.wfonts.com/download/data/2014/12/30/lucida-sans-typewriter/lucida-sans-typewriter.zip" "https://www.wfonts.com/download/data/2015/10/29/lucida-grande/lucida-grande.zip" "https://www.wfonts.com/download/data/2014/12/30/lucida-calligraphy/lucida-calligraphy.zip" "https://www.wfonts.com/download/data/2014/12/30/lucida-fax/lucida-fax.zip" "https://www.wfonts.com/download/data/2016/05/14/lucida-bright/lucida-bright.zip" "https://www.ffonts.net/Lucida-Console.font.zip")
 	install "lucida" "${fonts[@]}"
+}
+
+letter() {
+	fonts=("https://www.wfonts.com/download/data/2016/07/10/letter-gothic-std/letter-gothic-std.zip")
+	install "letter" "${fonts[@]}"
 }
 
 input() {
 	fonts=("http://input.fontbureau.com/build/?fontSelection=whole&a=0&g=ss&i=0&l=0&zero=nodot&asterisk=0&braces=0&preset=default&line-height=1.2&accept=I+do&email=")
 	install "input" "${fonts[@]}"
+}
 
+terminus() {
+	fonts=("https://files.ax86.net/terminus-ttf/files/latest.zip")
+	install "terminus" "${fonts[@]}"
+}
+
+computer_modern() {
+	fonts=("http://mirrors.ctan.org/fonts/cm-unicode.zip")
+	install "computer_modern" "${fonts[@]}"
+}
+
+ibm() {
+	fonts=("https://github.com/IBM/plex/releases/download/v1.4.1/TrueType.zip")
+	install "ibmplex" "${fonts[@]}"
+}
+
+bitter() {
+	fonts=("https://www.huertatipografica.com/free_download/48")
+	install "bitter" "${fonts[@]}"
+}
+
+copy() {
+	name=$1
+	shift
+	mkdir -p $DEST/$name
+        find ./ -type f \( -iname \*.ttf -o -iname \*.otf \) -exec cp {} $DEST/$name \;
+        find ./ -type f \( -iname \*.TTF -o -iname \*.OTF \) -exec cp {} $DEST/$name \;	
 }
 
 install() {
 	name=$1
-	shift	
+	shift
 	fonts="$@"
-	for f in $fonts; do 
-		wget $f
+	for i in "${!fonts[@]}"; do
+		wget -O $i.zip ${fonts[$i]}
+		unzip -o $i.zip
+		copy "$name"
+		rm $i.zip
 	done
-	for i in $(ls -1 *.zip); do
-		unzip $i
-	done
-	mkdir -p $DEST/$name
-	find ./ -type f \( -iname \*.ttf -o -iname \*.otf \) -exec cp {} $DEST/$name \;
-	find ./ -type f \( -iname \*.TTF -o -iname \*.OTF \) -exec cp {} $DEST/$name \;
-}
-
-# install ibm plex fonts
-
-ibm() {
-	git clone https://github.com/IBM/plex
-	mkdir -p $DEST/ibm
-	find ./ -type f \( -iname \*.otf \) -exec cp {} $DEST/$name \;
 }
 
 # install go font
 go() {
-	git clone https://go.googlesource.com/image
-	mkdir -p $DEST/go
-	cp image/font/gofont/ttfs/* $DEST/go
+	fonts=("https://go.googlesource.com/image/+archive/master/font/gofont/ttfs.tar.gz")
+	wget -O gofont.tar.gz ${fonts[0]}
+	copy "go"
 }
 
-# install terminus font
-terminus() {
-	wget https://files.ax86.net/terminus-ttf/files/latest.zip
-	unzip latest.zip
-	cd terminus-ttf-4.46.0
-	mkdir -p $DEST/terminus
-	cp *.ttf $DEST/terminus
-}
 
-# install cmu fonts
-computer_modern() {
-	wget http://mirrors.ctan.org/fonts/cm-unicode.zip
-	unzip cm-unicode.zip
-	mkdir -p $DEST/computer-modern
-	find ./ -type f \( -iname \*.ttf -o -iname \*.otf \) -exec cp {} $DEST/$name \;
-	find ./ -type f \( -iname \*.TTF -o -iname \*.OTF \) -exec cp {} $DEST/$name \;
-}
-
-bitter() {
-	wget -O bitter.zip https://www.huertatipografica.com/free_download/48
-	unzip bitter.zip
-	mkdir -p $DEST/bitter
-	cp *.otf $DEST/bitter
-}
-
-FONTS=(lucida syntax go terminus computer_modern bitter ibm)
+FONTS=(input letter lucida syntax go terminus computer_modern bitter ibm)
 
 for font in ${FONTS[@]}; do
 	tmp=$(mktemp -d)
