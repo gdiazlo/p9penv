b6baa967ae685984651077cca6497ce93a03f709
Gabriel Diaz <gdiazlo@qswarm.com>
2022-05-02 19:37:41 +0200

    update wezterm config

--- desktop/wezterm.lua
+++ desktop/wezterm.lua
@@ -1,7 +1,20 @@
 local wezterm = require 'wezterm';
 return {
 	font = wezterm.font("IBM Plex Mono"),
-	font_size = 14,
-	color_scheme = "IC_Orange_PPL",
+	font_size = 13,
+	color_scheme = "AtomOneLight",
 	use_ime = true,
+  window_frame = {
+
+    font = wezterm.font({family="IBM Plex Sans", weight="Bold"}),
+    font_size = 13.0,
+    active_titlebar_bg = "#f4f4f4",
+    inactive_titlebar_bg = "#333333",
+  },
+  launch_menu = {
+        {
+          label = "Plan9",
+          args = {"/usr/local/plan9/bin/rc","-l"},
+        },
+    },
 }
